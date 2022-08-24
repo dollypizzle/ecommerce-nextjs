80959957e5edce4114da4fa9de4fb840e17d22d8
dollypizzle <dolapo.mabawonku@gmail.com>
2022-08-24 18:23:13 +0100

    Fixed bugs

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import flashMessages from './flashMessages';
 import auth from './auth';
 import cart from './cart';
 
 export default combineReducers({
-  flashMessages,
   auth,
   cart,
 });
